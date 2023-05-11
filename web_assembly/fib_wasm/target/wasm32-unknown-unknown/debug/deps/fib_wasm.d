df658cf570f2fe89336987f499553073cf20e55e
ItsRareToSeeIt <itsraretoseeit@proton.me>
2023-05-12 00:13:00 +0200

    use rust to design a triangle in js

---
+++ web_assembly/fib_wasm/target/wasm32-unknown-unknown/debug/deps/fib_wasm.d
@@ -0,0 +1,5 @@
+/Users/mgalazka/dev/rust/web_assembly/fib_wasm/target/wasm32-unknown-unknown/debug/deps/fib_wasm.wasm: src/lib.rs
+
+/Users/mgalazka/dev/rust/web_assembly/fib_wasm/target/wasm32-unknown-unknown/debug/deps/fib_wasm.d: src/lib.rs
+
+src/lib.rs:
---
+++ web_assembly/fib_wasm/target/wasm32-unknown-unknown/debug/fib_wasm.d
@@ -0,0 +1,1 @@
+/Users/mgalazka/dev/rust/web_assembly/fib_wasm/target/wasm32-unknown-unknown/debug/fib_wasm.wasm: /Users/mgalazka/dev/rust/web_assembly/fib_wasm/src/lib.rs
