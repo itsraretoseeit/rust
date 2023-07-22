5ab8e55a007d0a251811f4d8623c45790511a150
ItsRareToSeeIt <itsraretoseeit@proton.me>
2023-07-22 23:11:04 +0200

    commit before cleanup

---
+++ web_assembly/fib_wasm/target/debug/deps/fib_wasm-36d25cde0d5d446b.d
@@ -0,0 +1,5 @@
+/Users/mgalazka/dev/rust/web_assembly/fib_wasm/target/debug/deps/fib_wasm-36d25cde0d5d446b.rmeta: src/lib.rs
+
+/Users/mgalazka/dev/rust/web_assembly/fib_wasm/target/debug/deps/fib_wasm-36d25cde0d5d446b.d: src/lib.rs
+
+src/lib.rs:
---
+++ web_assembly/fib_wasm/target/debug/deps/fib_wasm-af0c0e915f2865c7.d
@@ -0,0 +1,5 @@
+/Users/mgalazka/dev/rust/web_assembly/fib_wasm/target/debug/deps/fib_wasm-af0c0e915f2865c7.rmeta: src/lib.rs
+
+/Users/mgalazka/dev/rust/web_assembly/fib_wasm/target/debug/deps/fib_wasm-af0c0e915f2865c7.d: src/lib.rs
+
+src/lib.rs:
